0089304e27e4383297efc576dc26eddcb7eb4ac8
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-09 20:49:33 +0700

    Sửa biến status, thêm hàm setAllPixels đặt màu cho tất cả LED trong dải

--- LED_control.ino
+++ LED_control.ino
@@ -9,7 +9,7 @@
 // Đối tượng NeoPixel để điều khiển LED
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-String status = "OFF";                     // Trạng thái đèn: "ON" hoặc "OFF"
+bool status = 0;                     // Trạng thái đèn: "ON" hoặc "OFF"
 String color = "#000000";                  // Mã màu LED dưới dạng hex
 int brightness = 100;                      // Độ sáng LED (0-255)
 
@@ -23,4 +23,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+// Hàm đặt màu cho tất cả LED trong dải
+void setAllPixels(uint32_t color) {
+  for (int i = 0; i < NUMPIXELS; i++) {     // Duyệt qua từng LED
+    strip.setPixelColor(i, color);          // Đặt màu cho LED
+  }
+  strip.show();                             // Hiển thị màu sắc mới
 }
